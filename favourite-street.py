a3845935c9610f1b2ff3a77779af2bdfe44a085b
isolovey-robarts <isolovey@robarts.ca>
2019-11-07 15:59:02 -0500

    My initial commit. Works great!

---
+++ favourite-street.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+
+with open(sys.argv[1]) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        print("{}'s favourite street is {}.".format(row[1],row[2]))
+
+print('Done.')
