68d07ea1598404b3f92ae40ccb84cc259b2be193
Igor <isolove@uwo.ca>
2019-11-08 11:29:24 -0500

    Skip first line and edit street name. Fixes #1

--- favourite-street.py
+++ favourite-street.py
@@ -7,6 +7,7 @@ with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
+        if linecount > 0:
         print("{}'s favourite street is {}.".format(row[1],row[2]))
 
 print('Done.')
